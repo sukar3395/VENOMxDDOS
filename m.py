15c1c6ce5fc648d51d0a34fd5a575c207c91c9a8
VENOMXDOP <venomcrazy913@gmail.com>
2024-06-22 19:10:16 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import datetime
 import os
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('7255048090:AAE-cpQfymsCuURuxkfB-TmGCJBeG9p7knM')
 
 # Admin user IDs
-admin_id = ["YOUR ID"]
+admin_id = ["5588464519"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
